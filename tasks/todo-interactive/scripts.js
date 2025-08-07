b440bf0a8f487e21edd3f4b995f11dbe6cfeb7b3
abnakore <abdullahinakore@gmail.com>
2025-08-07 19:29:50 +0100

    init

--- tasks/todo-interactive/scripts.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const list = document.querySelectorAll("li");
-
-  list.forEach((item) =>
-    item.addEventListener("click", () => item.classList.toggle("completed"))
-  );
-});
